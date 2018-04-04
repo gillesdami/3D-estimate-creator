3944a1f5b3dd30684ba4aee5b70c40ea04f4b966
Damien Gilles <pro@gillesdamien.com>
2018-04-04 12:24:25 +0200

    added vuejs

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,12 @@
 import { call } from 'redux-saga/effects';
+import Vue from 'vue';
+import App from '../vues/App.vue';
 
 export function* initSaga() {
     yield call(console.log, "hello world !");
+
+    new Vue({
+        el: 'body',
+        render: h => h(App)
+    });
 }
