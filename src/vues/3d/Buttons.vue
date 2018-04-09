17336524f5d4e311774236a4512ef5c2261915b8
Alex1664 <alex.vacheret@free.fr>
2018-04-09 13:38:04 +0200

    add helper / trash buttons views

---
+++ src/vues/3d/Buttons.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="buttons">
+        <img v-on:click="help" id="questionMark" src="../../../assets/questionMark.jpg"/>
+        <img v-on:click="deleteAll" id="trash" src="../../../assets/trash.png"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "buttons",
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App!'
+            }
+        },
+        methods: {
+            help: function () {
+                alert('Display help panel')
+            },
+            deleteAll: function () {
+                alert('Delete all ...')
+            }
+        }
+    }
+</script>
+
+<style lang=css>
+    #buttons {
+        margin: 0.5% 0 0 0.5%
+    }
+
+    #questionMark {
+        height: 3%;
+        width: 2%;
+    }
+
+    #trash {
+        height: 3%;
+        width: 2%;
+    }
+</style>
--- src/vues/App.vue
+++ src/vues/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-  </div>
+    <div id="app">
+        <buttons/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App!'
+    import Buttons from './3d/Buttons'
+    export default {
+        name: 'app',
+        components : {
+            'buttons' : Buttons
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App!'
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="css">
-  #app {
-    color: #56b983;
-  }
+    #app {
+        color: #56b983;
+    }
 </style>
