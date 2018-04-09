f6be8de06aa45ec7f7c1602d085ab35d183da415
Julien Osouf <osoufjulie@eisti.eu>
2018-04-09 14:19:51 +0200

    new components

---
+++ src/vues/3d/General.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="general">
+        <p> Nom : <span> {{object.nom}}</span></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "General",
+        props: ['object']
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/vues/Total.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="total">
+        <p> Total : {{totalValue}} € </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Total",
+        props: ['totalValue']
+    }
+</script>
+
+<style scoped>
+    #total {
+        font-weight: bold;
+    }
+</style>
