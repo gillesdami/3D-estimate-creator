615e22b788cf45eb6a24b4beefd4d2e98e3ee46d
Alex1664 <alex.vacheret@free.fr>
2018-08-21 15:46:11 +0200

    [S2.7] Modifier un objet
    Ajout options / regles pour verifier les modifications des objets

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,4 +1,4 @@
-const toArray = obj => 
+const toArray = obj =>
     Object.keys(obj).map(key => {
         obj[key]["name"] = key;
         return obj[key]
@@ -14,9 +14,20 @@ const toMap = arr => {
     return res;
 };
 
+const checkUniqueApparel = root => {
+    let res = null;
+    root.forEach(e => {
+        let apparels = [];
+        e.apparels.forEach(ap => apparels.push(ap.type));
+        if (apparels.find((element, index) => (apparels.indexOf(element) !== index))) res = e;
+    });
+
+    return res;
+};
+
 (async function () {
     const initialState = await (await fetch('/objectsAvailable.json')).json();
-    
+
     const JSONEditorOptions = {
         schema: {
             type: "array",
@@ -25,7 +36,10 @@ const toMap = arr => {
                 headerTemplate: "{{self.name}}",
                 type: "object",
                 properties: {
-                    activated: {type: "boolean"},
+                    activated: {
+                        type: "boolean",
+                        format: "checkbox"
+                    },
                     apparels: {
                         type: "array",
                         items: {
@@ -38,19 +52,29 @@ const toMap = arr => {
                                 },
                                 values: {
                                     type: "array",
-                                    items: {type: "string"}
+                                    items: {
+                                        type: "string",
+                                        minLength: 1
+                                    }
                                 }
-                            }
+                            },
+                            options: {
+                                "collapsed": true
+                            },
                         }
                     },
-                    category: {type: "string"},
+                    category: {
+                        type: "string",
+                        minLength: 1
+                    },
                     name: {
                         type: "string",
                         links: [{
                             rel: "image",
                             href: "/models/{{self}}/image.jpg",
                             mediaType: "image/jpg"
-                        }]
+                        }],
+                        minLenght: 1
                     },
                     price: {
                         type: "object",
@@ -67,14 +91,20 @@ const toMap = arr => {
                             "NORMANDIE": {type: "number"},
                             "HAUTS DE FRANCE": {type: "number"},
                             "BRETAGNE": {type: "number"}
-                        }
+                        },
+                        options: {
+                            "collapsed": true
+                        },
                     },
-                    section: {type: "string"},
-                    
+                    section: {
+                        type: "string",
+                        minLength: 1
+                    },
                     settings: {
                         type: "array",
                         items: {
                             type: "object",
+                            headerTemplate: "{{self.type}}",
                             properties: {
                                 type: {
                                     type: "string",
@@ -83,19 +113,22 @@ const toMap = arr => {
                                 value: {
                                     type: "object",
                                     properties: {
-                                        "Toit pagode": {type: "number"}, 
-                                        "Rideau": {type: "number"}, 
-                                        "Plancher": {type: "number"}, 
-                                        "Lestage": {type: "number"}, 
-                                        "Pignon": {type: "number"}, 
-                                        "Croix de saint andre": {type: "number"}, 
-                                        "Barre de pignon": {type: "number"}, 
-                                        "Toit travee": {type: "number"}, 
-                                        "Rideau Longueur": {type: "number"}, 
+                                        "Toit pagode": {type: "number"},
+                                        "Rideau": {type: "number"},
+                                        "Plancher": {type: "number"},
+                                        "Lestage": {type: "number"},
+                                        "Pignon": {type: "number"},
+                                        "Croix de saint andre": {type: "number"},
+                                        "Barre de pignon": {type: "number"},
+                                        "Toit travee": {type: "number"},
+                                        "Rideau Longueur": {type: "number"},
                                         "Rideau Largeur": {type: "number"}
                                     }
                                 }
-                            }
+                            },
+                            options: {
+                                "collapsed": true
+                            },
                         }
                     },
                     files: {
@@ -112,11 +145,36 @@ const toMap = arr => {
         },
         startval: toArray(initialState),
         no_additional_properties: true,
-        required_by_default: true
+        required_by_default: true,
+        disable_array_delete_last_row: true,
     };
-    
+
     const jsonDOM = document.querySelector("json");
     const editor = new JSONEditor(jsonDOM, JSONEditorOptions);
+
+    editor.on('change', function () {
+        let errors = editor.validate();
+        const elementWithNoUniqueApparels = checkUniqueApparel(editor.getValue());
+
+        if (elementWithNoUniqueApparels != null) {
+            alert('L\'item : ' + elementWithNoUniqueApparels.name + ' a deux apparels du meme type.' +
+                ' Merci de modifier au moins un des deux types');
+            errors.push({
+                message: "apparels identiques sur un item",
+                property: elementWithNoUniqueApparels
+            })
+        }
+
+        if (errors.length) {
+            // errors is an array of objects, each with a `path`, `property`, and `message` parameter
+            // `property` is the schema keyword that triggered the validation error (e.g. "minLength")
+            // `path` is a dot separated path into the JSON object (e.g. "root.path.to.field")
+            console.log(errors);
+            document.getElementById("save").disabled = true;
+        } else {
+            document.getElementById("save").disabled = false;
+        }
+    });
 
     document.getElementById("save").addEventListener("click", () => {
         const json = toMap(editor.getValue());
