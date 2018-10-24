1e40f11ca3389b311e3496619522de29a09595eb
Alex1664 <alex.vacheret@free.fr>
2018-10-24 16:36:17 +0200

    add span to cart / update price with span

--- src/vues/TotalMainPage.vue
+++ src/vues/TotalMainPage.vue
@@ -8,7 +8,7 @@
 
 <script>
     import { $select } from '../sagas/vue';
-    import { objectsDisplayed, totalSelector } from '../selectors';
+    import { getSpansState, objectsDisplayed, totalSelector } from '../selectors';
 
     export default {
         name: "Total",
@@ -16,17 +16,33 @@
             total: function () {
                 let totalItems = $select(totalSelector, window.objectsAvailable);
                 const objDisplayed = $select(objectsDisplayed, window.objectsAvailable);
+                const spanState = $select(getSpansState);
 
                 let totalApparels = 0;
 
                 objDisplayed.forEach(obj => {
+
                     obj.apparels.forEach(ap => {
                         if (!ap.value.name.includes("aucun"))
                             totalApparels += ap.value.price["ILE DE FRANCE"];
                     });
+
+                    // Gestion des travées
+                    const item = spanState.filter((span) => obj.uid === span.uid);
+                    if (item.length !== 0) {
+
+                        totalItems += window.objectsAvailable[obj.name].price["ILE DE FRANCE"] * item[0].spansNumber;
+                        totalItems += totalApparels * (item[0].spansNumber + 1);
+                    } else {
+                        totalItems += totalApparels;
+                    }
+
+                    totalApparels = 0;
+
                 });
 
-                return totalItems + totalApparels;
+
+                return totalItems;
             }
         }
     }
